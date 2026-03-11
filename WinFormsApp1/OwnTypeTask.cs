465f84f15a63095a05b58d6dba2924594b8351d1
Ph0sph0russas <danila.ustiuzhanin@gmail.com>
2026-03-11 14:28:36 +0800

    Добавлены тесты

--- WinFormsApp1/OwnTypeTask.cs
+++ WinFormsApp1/OwnTypeTask.cs
@@ -19,6 +19,18 @@ namespace WinFormsApp1
             this.y = y;
             this.z = z;
         }
+        public double getX()
+        {
+            return this.x;
+        }
+        public double getY()
+        {
+            return this.y;
+        }
+        public double getZ()
+        {
+            return this.z;
+        }
         public double CalculateVectorLength(double x, double y, double z)
         {
             return Math.Sqrt( x * x + y * y + z * z);
---
+++ WinFormsApp1Tests/OwnTypeTaskTests.cs
@@ -0,0 +1,44 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using WinFormsApp1;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WinFormsApp1.Tests
+{
+    [TestClass()]
+    public class OwnTypeTaskTests
+    {
+        [TestMethod()]
+        public void OwnTypeTaskTest()
+        {
+            Assert.Fail();
+        }
+
+        [TestMethod()]
+        public void getXTest()
+        {
+            Assert.Fail();
+        }
+
+        [TestMethod()]
+        public void getYTest()
+        {
+            Assert.Fail();
+        }
+
+        [TestMethod()]
+        public void getZTest()
+        {
+            Assert.Fail();
+        }
+
+        [TestMethod()]
+        public void CalculateVectorLengthTest()
+        {
+            Assert.Fail();
+        }
+    }
+}
